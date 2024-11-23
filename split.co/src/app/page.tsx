4be5597b6c59734878bb3feb932d80cf5e04c4ad
Ananmay 125 <ananmay125@gmail.com>
2024-11-23 15:17:59 +0530

    bruh i hate this design i gotta improve

--- split.co/src/app/page.tsx
+++ split.co/src/app/page.tsx
@@ -2,100 +2,54 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <nav className="nav-bar-1">
+        <h2 className="justaheading">Split.co</h2>
+        <div className="nav-bar-div">
+          <img 
+            src="favicon.ico"
+            className="account-img"
+            width={35}
+            height={35}
+          ></img>
+          <input type="text" placeholder="Search" className="search-box"></input>
+          <p className="nav-bar-text">Home</p>
+          <p className="nav-bar-text">Favorites</p>
+          <p className="nav-bar-text">Friends</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </nav>
+      <div className="part-1">
+        <nav className="sidebar">
+            <ul className="sidebar-list">
+              <h3>General</h3>
+              <li className="sidebar-list-link">Friends</li>
+              <li className="sidebar-list-link">Favourites</li>
+              <li className="sidebar-list-link">Servers</li>
+              <li className="sidebar-list-link">Community</li>
+              <h3>Games</h3>
+              <li className="sidebar-list-link">Singleplayer</li>
+              <li className="sidebar-list-link">Co-op</li>
+              <li className="sidebar-list-link">paid</li>
+              <li className="sidebar-list-link">free</li>
+            </ul>
+        </nav>
+
+        <div className="display">
+          <img
+            src="image.png"
+            className="display-case-img"
+          ></img>
+          <div className="display-case-div">
+            <h1 className="display-case-heading">Terraria</h1>
+            <h3 className="display-case-text">2D minecraft....</h3>
+            <div className="tag-div">            
+              <p className="tags">2D</p><p className="tags">survival</p><p className="tags">open-world</p>
+            </div>
+          </div>
+        </div>
+      /</div>
+
+      <main></main>
+    </>
   );
 }
